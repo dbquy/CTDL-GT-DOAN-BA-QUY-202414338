fcc41e21a988f74439ea01dcf17cbd22a7476147
dbquy <doanbaquy26@gmail.com>
2026-05-20 20:54:49 +0700

    Cay day du, cay lech phai, cay lech trai - kieu moc noi

--- Btap tuan 11/cautruccay_mocnoi.cpp
+++ Btap tuan 11/cautruccay_mocnoi.cpp
@@ -56,6 +56,100 @@ void sau(Node* t) {
 
 int main() {
 
+    // CAI DAT CAU TRUC CAY VOI BIEU THUC (a*b+c)/d - (e*f-h)^g
+   
+   
+    //Cay day du
+    
+    Node* a = tao('-');
+
+    a->left = tao('/');
+    a->right = tao('^');
+
+    a->left->left = tao('+');
+    a->left->right = tao('d');
+
+    a->right->left = tao('-');
+    a->right->right = tao('g');
+
+    a->left->left->left = tao('*');
+    a->left->left->right = tao('c');
+
+    a->right->left->left = tao('*');
+    a->right->left->right = tao('h');
+
+    a->left->left->left->left = tao('a');
+    a->left->left->left->right = tao('b');
+
+    a->right->left->left->left = tao('e');
+    a->right->left->left->right = tao('f');
+
+    cout << "CAY DAY DU\n";
+
+    cout << "\nduyet truoc: ";
+    truoc(a);
+
+    cout << "\nduyet giua: ";
+    giua(a);
+
+    cout << "\nduyet sau: ";
+    sau(a);
+
+   
+    //Cay Lech Trai
+  
+    Node* b = tao('-');
+
+    b->left = tao('/');
+
+    b->left->left = tao('+');
+
+    b->left->left->left = tao('*');
+
+    b->left->left->left->left = tao('a');
+    b->left->left->left->right = tao('b');
+
+    b->left->left->right = tao('c');
+
+    b->left->right = tao('d');
+
+    cout << "\n\nCAY LECH TRAI\n";
+
+    cout << "\nduyet truoc: ";
+    truoc(b);
+
+    cout << "\nduyet giua: ";
+    giua(b);
+
+    cout << "\nduyet sau: ";
+    sau(b);
+
+    
+    //Cay Lech Phai
+    
+
+    Node* c = tao('-');
+
+    c->right = tao('^');
+
+    c->right->right = tao('-');
+
+    c->right->right->right = tao('*');
+
+    c->right->right->right->left = tao('e');
+    c->right->right->right->right = tao('f');
+
+    cout << "\n\nCAY LECH PHAI\n";
+
+    cout << "\nduyet truoc: ";
+    truoc(c);
+
+    cout << "\nduyet giua: ";
+    giua(c);
+
+    cout << "\nduyet sau: ";
+    sau(c);
+    
 
     return 0;
 }
