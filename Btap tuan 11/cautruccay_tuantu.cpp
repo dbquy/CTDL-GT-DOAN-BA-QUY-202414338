2bee317a8db1760e090ff262563378b2290759d8
dbquy <doanbaquy26@gmail.com>
2026-05-20 19:53:42 +0700

    sua lai bieu thuc theo yeu cau de bai, finish kieu tuan tu

--- Btap tuan 11/cautruccay_tuantu.cpp
+++ Btap tuan 11/cautruccay_tuantu.cpp
@@ -36,73 +36,115 @@ void sau(char a[], int i) {
     cout << a[i] << " ";
 }
 int main() {
-
+    // CAI DAT CAU TRUC CAY VOI BIEU THUC (a*b+c)/d - (e*f-h)^g
     // Cay day du
 
-    char a[20] = {};
+    char a[50] = {};
 
-    a[1] = 'A';
-    a[2] = 'B';
-    a[3] = 'C';
-    a[4] = 'D';
-    a[5] = 'E';
-    a[6] = 'F';
-    a[7] = 'G';
+    a[1] = '-';
+
+    a[2] = '/';
+    a[3] = '^';
+
+    a[4] = '+';
+    a[5] = 'd';
+
+    a[6] = '-';
+    a[7] = 'g';
+
+    a[8] = '*';
+    a[9] = 'c';
+
+    a[12] = '*';
+    a[13] = 'h';
+
+    a[16] = 'a';
+    a[17] = 'b';
+
+    a[24] = 'e';
+    a[25] = 'f';
 
     cout << "CAY DAY DU\n";
 
-    for (int i = 1; i <= 7; i++) {
-        cout << i << " : " << a[i] << endl;
-    }
+    cout << "\nduyet truoc: ";
+    truoc(a, 1);
+
+    cout << "\nduyet giua: ";
+    giua(a, 1);
+
+    cout << "\nduyet sau: ";
+    sau(a, 1);
     
     // Cay lech trai
 
-    char b[20] = {};
+    char b[50] = {};
 
-    b[1] = 'A';
-    b[2] = 'B';
-    b[4] = 'C';
-    b[8] = 'D';
+    b[1] = '-';
+    b[2] = '/';
+    b[4] = '+';
+    b[8] = '*';
+    b[16] = 'a';
+    b[17] = 'b';
+    b[9] = 'c';
+    b[5] = 'd';
 
-    cout << "\nCAY LECH TRAI\n";
+    cout << "\n\nCAY LECH TRAI\n";
 
-    cout << "1 : " << b[1] << endl;
-    cout << "2 : " << b[2] << endl;
-    cout << "4 : " << b[4] << endl;
-    cout << "8 : " << b[8] << endl;
+    cout << "\nduyet truoc: ";
+    truoc(b, 1);
+
+    cout << "\nduyet giua: ";
+    giua(b, 1);
+
+    cout << "\nduyet sau: ";
+    sau(b, 1);
 
     
     //Cay lech phai
 
-    char c[20] = {};
+     char c[50] = {};
 
-    c[1] = 'A';
-    c[3] = 'B';
-    c[7] = 'C';
-    c[15] = 'D';
+    c[1] = '-';
+    c[3] = '^';
+    c[7] = '-';
+    c[15] = '*';
 
-    cout << "\nCAY LECH PHAI\n";
+    c[30] = 'e';
+    c[31] = 'f';
 
-    cout << "1 : " << c[1] << endl;
-    cout << "3 : " << c[3] << endl;
-    cout << "7 : " << c[7] << endl;
-    cout << "15 : " << c[15] << endl;
+    cout << "\n\nCAY LECH PHAI\n";
+
+    cout << "\nduyet truoc: ";
+    truoc(c, 1);
+
+    cout << "\nduyet giua: ";
+    giua(c, 1);
+
+    cout << "\nduyet sau: ";
+    sau(c, 1);
     
     //Cay zigzac
 
-    char d[20] = {};
+    char d[50] = {};
 
-    d[1] = 'A';
-    d[3] = 'B';
-    d[6] = 'C';
-    d[13] = 'D';
+    d[1] = '-';
+    d[3] = '/';
+    d[6] = '+';
+    d[13] = '*';
 
-    cout << "\nCAY ZIGZAC\n";
+    d[26] = 'a';
+    d[27] = 'b';
 
-    cout << "1 : " << d[1] << endl;
-    cout << "3 : " << d[3] << endl;
-    cout << "6 : " << d[6] << endl;
-    cout << "13 : " << d[13] << endl;
+    cout << "\n\nCAY ZIGZAG\n";
+
+    cout << "\nduyet truoc: ";
+    truoc(d, 1);
+
+    cout << "\nduyet giua: ";
+    giua(d, 1);
+
+    cout << "\nduyet sau: ";
+    sau(d, 1);
 
    //Cay bieu thuc
     // (a*b+c)/d - (e*f-h)^g
