a66213792d42046bc762c32b56fe6a0f0af91d17
dbquy <doanbaquy26@gmail.com>
2026-05-26 22:44:42 +0700

    in ra trang thai luu tru cua cay

---
+++ Btap tuan 12/sapxepvundong.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// ham in mang
+void inMang(int a[], int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
+int main()
+{
+    
+    return 0;
+}
