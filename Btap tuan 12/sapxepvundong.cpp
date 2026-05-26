616e3e4daaf9305f84341a02e81933cbfaa6e3ff
dbquy <doanbaquy26@gmail.com>
2026-05-26 22:54:03 +0700

    tao ham vun dong cho cay

--- Btap tuan 12/sapxepvundong.cpp
+++ Btap tuan 12/sapxepvundong.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// ham in mang
+//ham in mang
 void inMang(int a[], int n)
 {
     for(int i = 1; i <= n; i++)
@@ -9,6 +9,41 @@ void inMang(int a[], int n)
         cout << a[i] << " ";
     }
     cout << endl;
+}
+
+//ham vun dong
+void vundong(int a[], int n, int i)
+{
+    int max = i;
+
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    //tim node lon nhat
+    if(left <= n && a[left] > a[max])
+    {
+        max = left;
+    }
+
+    if(right <= n && a[right] > a[max])
+    {
+        max = right;
+    }
+
+    //neu can doi cho
+    if(max != i)
+    {
+        int temp = a[i];
+        a[i] = a[max];
+        a[max] = temp;
+
+        //in trang thai luu tru sau moi lan doi
+        cout << "Trang thai sau khi doi: ";
+        inMang(a, n);
+
+        // vun tiep nhanh con
+        vundong(a, n, max);
+    }
 }
 
 
