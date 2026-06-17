37401a9846b74c0965ca553f108f76f40d01ef68
dbquy <doanbaquy26@gmail.com>
2026-06-17 18:38:15 +0700

    duyet do thi theo chieu rong

--- Btap tuan 15/Dothigiaothong_cautrucmangdinhke.cpp
+++ Btap tuan 15/Dothigiaothong_cautrucmangdinhke.cpp
@@ -24,10 +24,43 @@ void themCanh(int u, int v)
     a[u][v] = 1;
     a[v][u] = 1;
 }
-void themCanh(int u, int v)
+// Duyệt theo chiều rộng
+void duyettheochieurong(int start)
 {
-    a[u][v] = 1;
-    a[v][u] = 1;
+    bool daXet[11] = {false};
+
+    queue<int> q;
+
+    daXet[start] = true;
+    q.push(start);
+
+    int stt = 1;
+    while(p != NULL)
+        {
+            int v = p->data;
+
+            if(daXet[v] == false)
+            {
+                daXet[v] = true;
+                q.push(v);
+            }
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        cout << stt++ << ". " << ten[u] << endl;
+
+        for (int v = 0; v < 11; v++)
+        {
+            if (a[u][v] == 1 && daXet[v] == false)
+            {
+                daXet[v] = true;
+                q.push(v);
+            }
+        }
+    }
 }
 
 int main()
