15e16b73826df9970951d8518f54e971fe14e93a
dbquy <doanbaquy26@gmail.com>
2026-06-17 18:29:41 +0700

    them vao cau truc luu tru tuan tu

--- Btap tuan 15/Dothigiaothong_cautrucmangdinhke.cpp
+++ Btap tuan 15/Dothigiaothong_cautrucmangdinhke.cpp
@@ -2,7 +2,33 @@
 #include <queue>
 using namespace std;
 
-s
+string ten[11] = {
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
+
+int a[11][11] = {0};
+
+// them canh
+void themCanh(int u, int v)
+{
+    a[u][v] = 1;
+    a[v][u] = 1;
+}
+void themCanh(int u, int v)
+{
+    a[u][v] = 1;
+    a[v][u] = 1;
+}
 
 int main()
 {
