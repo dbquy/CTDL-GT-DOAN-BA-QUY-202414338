0b6468146ada87a60f6d66bd0dfd177fba0efb8b
dbquy <doanbaquy26@gmail.com>
2026-06-17 18:57:49 +0700

    cau truc luu tru moc noi, tao danh sach, node,...

---
+++ Btap tuan 15/Dothigiaothong_cautrucmocnoi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *ds[11];
+
+string ten[11] =
+{
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
