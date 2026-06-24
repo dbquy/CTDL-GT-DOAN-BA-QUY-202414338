33cc24a1e6a01bef66ae86c06e265bbdf46ab747
dbquy <doanbaquy26@gmail.com>
2026-06-24 18:25:43 +0700

    tao node va them cuoi vao dslk '

--- Btap tuan 16/Timduongdi.cpp
+++ Btap tuan 16/Timduongdi.cpp
@@ -23,3 +23,31 @@ string ten[11] =
     "Hoa Binh",
     "Son Tay"
 };
+
+// tao node moi
+Node *taoNode(int x)
+{
+    Node *p = new Node;
+    p->data = x;
+    p->next = NULL;
+    return p;
+}
+
+// them vao cuoi DSLK
+void themCuoi(Node *&head, int x)
+{
+    Node *p = taoNode(x);
+
+    if(head == NULL)
+    {
+        head = p;
+        return;
+    }
+
+    Node *q = head;
+
+    while(q->next != NULL)
+        q = q->next;
+
+    q->next = p;
+}
