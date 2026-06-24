384c32bc0f48a2fbff99b6cdd7efe98036ad1487
dbquy <doanbaquy26@gmail.com>
2026-06-24 18:24:35 +0700

    tao cau truc luu tru do thi

---
+++ Btap tuan 16/Timduongdi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *ds[11];
+
+string ten[11] =
+{
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
