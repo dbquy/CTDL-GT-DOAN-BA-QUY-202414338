2fde6432e79878c8d53d4c8671cceda06889f4ec
dbquy <doanbaquy26@gmail.com>
2026-06-29 20:15:06 +0700

    tao do thi cac tinh thanh pho

---
+++ Btap tuan 18/cay khung_PRIM _ KRUSKAL .cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+const int MAX = 11;
+const int VC = 1000000;
+
+string TenTinh[MAX] = {
+    "HN","TN","BN","BG","UB","HP","HD","HY","PL","HB","ST"
+};
+
+struct DoThi {
+    int n;
+    int a[MAX][MAX];
+};
+
+struct Canh {
+    int dau, cuoi, ts;
+};
+
+void TaoDoThi(DoThi &g, int n) {
+    g.n = n;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            g.a[i][j]=0;
+}
